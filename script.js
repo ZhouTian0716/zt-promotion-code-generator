e899fdda9acf0281a0d994b9dfcf74a579e5e94f
Zhou_Tian_MBP <egglord0716@gmail.com>
2023-06-28 17:34:32 +1000

    code test check passed

--- script.js
+++ script.js
@@ -1,47 +1,72 @@
-function random_storeId(storeIds) {
-  return storeIds[Math.floor(Math.random() * storeIds.length)];
+// We may keep this somewhere else, but for now it's here.
+const secret = "v&6HVd^A%83hsfDpE!XIcq_yje#Wxgw2oSB4($l?P9Z7On0i+@5MTbkGaLF)RUKQY1zJrCN*utm";
+
+// helper function to DECODE
+function getNumberFromCode(code) {
+  let number = 0;
+  for (let i = 0; i < code.length; i++) {
+    number = number * secret.length + secret.indexOf(code.charAt(i));
+  }
+  return number;
 }
 
-function random_transactionId(transactionIds) {
-  return transactionIds[Math.floor(Math.random() * transactionIds.length)];
+// helper function to ENCODE
+function getCodeFromNumber(number, length) {
+  let code = "";
+  while (number > 0) {
+    const remainder = number % secret.length;
+    code = secret.charAt(remainder) + code;
+    number = Math.floor(number / secret.length);
+  }
+
+  // in case the code is not long enough, pad it with the first character
+  while (code.length < length) {
+    code = secret.charAt(0) + code;
+  }
+  return code;
 }
 
-// TODO: Modify this function
-function generateShortCode(storeId, transactionId) {
-  // Logic goes here
-  var date = new Date().getTime();
-  var uuid = "x".replace(/[xy]/g, function (c) {
-    var appendedID = "" + storeId.toString().length + transactionId.toString().length + storeId + transactionId;
-    var sar = ["0", "00", "000", "0000", "00000", "000000", "0000000"];
-    var das = [1, 10, 100, 1000, 10000, 100000, 1000000];
-    var l = 9 - appendedID.length - 2;
-    var docketNumber = 0;
-    if ((appendedID.length = 9)) {
-      docketNumber = appendedID;
-    }
-    if (appendedID.length < 9) {
-      var randomnumber = Math.floor(Math.random() * (9 - appendedID.length - das[l + 1] + 1)) + das[l + 1];
-      docketNumber = "" + appendedID + (9 - appendedID.length) + randomnumber;
-    }
-    //return docketNumber;
-    return c == "x" ? docketNumber : 0o0;
-  });
-  return uuid;
+// ENCODE LOGICS HERER
+function generateShortCode(storeId, customerId) {
+  // we may want to add some validation for storeId and customerId
+  const storeCode = getCodeFromNumber(storeId, 2); // 2 char for storeId, 75^2 should be ok for [1,200]
+  const customerCode = getCodeFromNumber(customerId, 3); // 2 char for storeId, 75^3 should be ok for [1,10000]
+
+  const timestamp = Math.floor(new Date().getTime() / 1000); // 获取当前时间戳
+
+  // try to get the meaningful timestamp range smaller, 1687930042 here can be changed to any other number
+  const partsToStore = timestamp - 1687930042;
+
+  const timestampCode = getCodeFromNumber(partsToStore, 4); // 用4个字符存储映射后的时间戳
+
+  let result = storeCode + timestampCode + customerCode;
+
+  if (result.length > 9) {
+    throw new Error("Generated random string exceeds the maximum length of 9 secret.");
+  }
+
+  while (result.length < 9) {
+    let randomIndex = Math.floor(Math.random() * secret.length);
+    result += secret.charAt(randomIndex);
+  }
+
+  return result;
 }
 
-// TODO: Modify this function
+// DECODE LOGICS HERER
 function decodeShortCode(shortCode) {
-  // Logic goes here
-  var sLen = shortCode.substring(0, 1);
-  var tLen = shortCode.substring(1, 2);
-  var iLen = sLen + tLen;
+  const storeCode = shortCode.substr(0, 2);
+  const timestampCode = shortCode.substr(2, 4);
+  const customerCode = shortCode.substr(6, 3);
+
+  const storeId = getNumberFromCode(storeCode);
+  const customerId = getNumberFromCode(customerCode);
+  const timestampPrefix = getNumberFromCode(timestampCode) + 1687930042; // reverse the timestamp
+
   return {
-    storeId: parseInt(shortCode.substring(iLen.length, parseInt(sLen, 10) + iLen.length), 10), // store id goes here,
-    shopDate: new Date(), // the date the customer shopped,
-    transactionId: parseInt(
-      shortCode.substring(parseInt(sLen, 10) + iLen.length, iLen.length + parseInt(sLen, 10) + parseInt(tLen, 10)),
-      10
-    ), // transaction id goes here
+    storeId: storeId,
+    transactionId: customerId,
+    shopDate: new Date(timestampPrefix * 1000),
   };
 }
 
